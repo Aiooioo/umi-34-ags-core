322f0b95875cd6ea0163e2a7f68e835a3e5bc56b
yanwh <yanwh@geoscene.cn>
2021-03-10 17:25:57 +0800

    update umi rc

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  ignoreMomentLocale: true,
+  routes: [{ path: '/', component: '@/pages/index' }],
   webpack5: {},
   fastRefresh: {},
 });
